fa51459229d890532e9f896fb7540f8d3bfb6040
ben <ndaloben8@gmail.com>
2023-10-29 21:03:05 +0300

    crud operation complete

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for real_estate project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from listings.views import (
+    listing_list, 
+    listing_retrieve, 
+    listing_create,
+    listing_update,
+    listing_delete
+    )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listing_list),
+    path('listing/<pk>/', listing_retrieve),
+    path('listing/<pk>/edit/', listing_update),
+    path('listing/<pk>/delete/', listing_delete),
+    path('add-listing/', listing_create),
 ]
+
